67f06ea8ca553074b45d82e3c487cd13a2c035f4
Selim Ouirari <selim.ouirari@linrob.io>
2023-10-23 10:12:52 +0200

    Feat: idea on robot hardware interface

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='robot_hardware_interface_pkg',
+    version='0.1.0',
+    packages=['robot_hardware_interface_pkg'],
+    # ... (other setup parameters)
+)
